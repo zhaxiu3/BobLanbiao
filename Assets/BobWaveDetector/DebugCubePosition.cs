5d39a67bad6de514a1c08dfc4b5d75abdbf37fcf
zhaxiu3 <zhaxiu3@163.com>
2014-01-20 18:27:33 +0800

    hehe

---
+++ Assets/BobWaveDetector/DebugCubePosition.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugCubePosition : MonoBehaviour {
+    TimedBuffer<Vector3> mPosition;
+    TimedBuffer<Vector3> mVelocity;
+
+    public float interval = 1f;
+	// Use this for initialization
+	void Start () {
+        mPosition = new TimedBuffer<Vector3>(interval);
+        mVelocity = new TimedBuffer<Vector3>(interval);
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        mPosition.AddData(this.transform.position);
+        if (mPosition.Count > 1)
+        {
+            mVelocity.AddData(mPosition[mPosition.Count-1] - mPosition[mPosition.Count - 2]);
+        }
+        else
+        {
+            mVelocity.AddData(Vector3.zero);
+        }
+        bool changed = false;
+        for (int i = 1; i < mVelocity.Count; i++)
+        {
+            if (mVelocity[i].x * mVelocity[i - 1].x < 0)
+            {
+                for (int j = 0; j < i; j++)
+                {
+
+                }
+            }
+        }
+	}
+}
---
+++ Assets/BobWaveDetector/TimedBuffer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TimedBuffer<T> {
+
+    public List<T> mdataBuffer;
+    public List<float> mtimeStamp;
+    private float minterval = 1f;
+
+    public int Count
+    {
+        get
+        {
+            return mdataBuffer.Count;
+        }
+    }
+    public TimedBuffer(float interval)
+    {
+        mdataBuffer = new List<T>();
+        mtimeStamp = new List<float>();
+    }
+
+    public T this[int i]
+    {
+        get
+        {
+            return mdataBuffer[i];
+        }
+    }
+
+    public void AddData(T data)
+    {
+        mdataBuffer.Add(data);
+        mtimeStamp.Add(Time.time);
+        if (Time.time - mtimeStamp[0] > minterval)
+        {
+            mdataBuffer.RemoveAt(0);
+            mtimeStamp.RemoveAt(0);
+        }
+    }
+    public void Clear()
+    {
+        mdataBuffer.Clear();
+        mtimeStamp.Clear();
+    }
+}
